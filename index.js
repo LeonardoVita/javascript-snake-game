cc17b9ed8ab787bb310a86cdb12277b1d528d8e5
Leonardo Vita Milanez de Almeida <Leonardo.vita0606@gmail.com>
2021-03-11 20:21:21 -0300

    colision with snake body

--- index.js
+++ index.js
@@ -43,11 +43,19 @@ function update(event){
 document.addEventListener("keydown", update);
 
 function playGame(){
-
+  
+  
   if(snake[0].x > 15 * box && direction === "right") snake[0].x = 0;
   if(snake[0].x < 0 * box && direction === "left") snake[0].x = 16 * box;
   if(snake[0].y > 15 * box && direction === "down") snake[0].y = 0;
   if(snake[0].y < 0 * box && direction === "up") snake[0].y = 16 * box;
+  
+  for(i=1; i < snake.length; i++){
+    if(snake[0].x === snake[i].x && snake[0].y === snake[i].y ){
+      clearInterval(jogo);
+      alert("PERDEU O JOGO! :(");
+    }
+  }
 
   createBackground();
   createSnake();
