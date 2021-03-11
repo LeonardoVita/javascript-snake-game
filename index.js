e02c4adac7a5a8cf18749c94c16e3d59d888760f
Leonardo Vita Milanez de Almeida <Leonardo.vita0606@gmail.com>
2021-03-11 20:11:47 -0300

    grow up snake with food

--- index.js
+++ index.js
@@ -73,7 +73,12 @@ function playGame(){
       break;
   }
   
-  snake.pop();
+  if(snakeX !== food.x || snakeY !== food.y){
+    snake.pop();
+  }else{
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
 
   let newHead = {
     x: snakeX,
