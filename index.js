38e3ad8cec0bf629ec00a15f468bdb9d1ad32b45
Leonardo Vita Milanez de Almeida <Leonardo.vita0606@gmail.com>
2021-03-11 19:06:01 -0300

    snake movement

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ snake[0] = {
   y: 8 * box,
 }
 
+let direction = "right"
+
 function createBackground(){
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
@@ -20,5 +22,37 @@ function createSnake(){
   }
 }
 
-createBackground();
-createSnake();
+function playGame(){
+  createBackground();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  switch (direction) {
+    case "right":
+      snakeX += box;
+      break;
+    case "left":
+      snakeX -= box;
+      break;
+    case "up":
+      snakeY -= box;
+      break;
+    case "down":
+      snakeX += box;
+      break; 
+    default:
+      break;
+  }
+  
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  }
+  snake.unshift(newHead);
+}
+
+const jogo = setInterval(playGame, 100);
